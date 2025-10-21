b9f7a78b0137d85f2250e8f5193a81ce34981bac
Bisendo <kimotobidaus@gmail.com>
2025-10-21 13:26:27 +0300

    some features are added on this pages

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,10 @@ import SignUpForm from "./components/SignUpToWatukazi";
 import LoginForm from "./components/SignInToWatukazi";
 import { LanguageProvider } from "./components/LanguageContext";
 import { ThemeProvider } from "./components/ThemeContext";
-import Service from "./components/Service";
 import ContactPage from "./components/Contacts";
 import AboutUs from "./components/AboutUs";
+import ServiceList from "./components/ServiceLists";
+import ServiceDetail from "./components/ServiceDetails";
 
 
 const App = () => {
@@ -26,28 +27,29 @@ const App = () => {
   return (
     <div>
       <ThemeProvider>
-      <LanguageProvider>
-        <Navibar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hero" element={<Hero />} />
-          <Route path="/joblist" element={<JobList />} />
-          <Route path="/jobcard" element={<JobCard />} />
-          <Route path="/animations" element={<AnimationWrapper />} />
-          <Route path="/footer" element={<Footer />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/Signin" element={<LoginForm />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/about" element={<AboutUs />} />
+        <LanguageProvider>
+          <Navibar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/hero" element={<Hero />} />
+            <Route path="/joblist" element={<JobList />} />
+            <Route path="/jobcard" element={<JobCard />} />
+            <Route path="/animations" element={<AnimationWrapper />} />
+            <Route path="/footer" element={<Footer />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/Signin" element={<LoginForm />} />
+            <Route path="/service/:id" element={<ServiceDetail />} />
+            <Route path="/service" element={<ServiceList />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutUs />} />
 
 
 
 
 
 
-        </Routes>
-      </LanguageProvider>
+          </Routes>
+        </LanguageProvider>
       </ThemeProvider>
     </div>
   );
